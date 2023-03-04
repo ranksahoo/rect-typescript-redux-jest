78446e5cc6ef7a77b21f26bd6d7d11a128c77ae6
Ranjan Sahoo <ranksahoo@outlook.com>
2023-03-04 23:24:08 +0530

    save changes

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -1,7 +1,10 @@
 import UserList from '@components/user/UserList'
 function Albums() {
+  //  const my_favorite_color = '#112C85'
+  /** xxzxzxzxzx */
   return (
     <div className='container mx-auto'>
+      {/* {my_favorite_color} */}
       <UserList />
     </div>
   )
